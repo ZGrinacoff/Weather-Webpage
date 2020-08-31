6f73a5c9236439be89e6ee4840db4463274dfed3
ZGrinacoff <zgrinacoff@gmail.com>
2020-08-30 17:08:04 -0700

    Finished

---
+++ Assets/data.py
@@ -0,0 +1,8 @@
+# # Dependency.
+# import pandas as pd
+
+# # Import csv.
+# df = pd.read_csv('../Resources/cities.csv')
+
+# # Export csv.
+# df.to_html('data.html', index=False, classes=['table', 'table-striped', 'table-hover'])
