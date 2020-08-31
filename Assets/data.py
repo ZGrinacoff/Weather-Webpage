ca6309cef9fd0b59126357ff5c5baca4499130df
Zachary Grinacoff <zgrinacoff@gmail.com>
2020-08-30 21:16:20 -0700

    Delete data.py

--- Assets/data.py
+++
@@ -1,8 +0,0 @@
-# # Dependency.
-# import pandas as pd
-
-# # Import csv.
-# df = pd.read_csv('../Resources/cities.csv')
-
-# # Export csv.
-# df.to_html('data.html', index=False, classes=['table', 'table-striped', 'table-hover'])
